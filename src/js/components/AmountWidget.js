7fa8d1422e69f75f4c900ef5d980151aef951523
Dominik Krawczyk <dominix.kr@gmail.com>
2022-04-22 18:22:20 +0200

    changes in project, partials added

---
+++ src/js/components/AmountWidget.js
@@ -0,0 +1,74 @@
+import {settings, select } from '../settings.js';
+
+class AmountWidget{
+  constructor(element){
+    const thisWidget = this;
+
+    thisWidget.getElements(element);
+    thisWidget.setValue(settings.amountWidget.defaultValue);
+    thisWidget.initActions();
+      
+    // console.log('AmountWidet:', thisWidget);
+    // console.log('constructor arguments:', element);
+  }
+
+  getElements(element){
+    const thisWidget = this;
+
+    thisWidget.element = element;
+    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+
+  }
+
+  setValue(value){
+    const thisWidget = this;
+
+    const newValue = parseInt(value);
+
+    /* TODO: Add validation */
+
+    if (thisWidget.value !== newValue && !isNaN(newValue) && settings.amountWidget.defaultMin <= newValue && settings.amountWidget.defaultMax >= newValue) {
+        
+      thisWidget.value = newValue;
+      // console.log(newValue);
+      thisWidget.announce();
+    }
+    thisWidget.value = newValue;
+    thisWidget.input.value = thisWidget.value;
+  }
+
+  initActions(){
+    const thisWidget = this;
+
+    thisWidget.input.addEventListener('change', function(){
+      thisWidget.setValue(thisWidget.input.value);
+      // console.log('change Work!:', thisWidget);
+    });
+
+    thisWidget.linkDecrease.addEventListener('click',function(event){
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value -1);
+      // console.log('Decrease Work!:', thisWidget);
+    });
+
+    thisWidget.linkIncrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value +1);
+      // console.log('increase Work!:', thisWidget);
+    });
+
+  }
+
+  announce(){
+    const thisWidget = this;
+
+    const event = new CustomEvent('updated', {
+      bubbles: true
+    });
+    thisWidget.element.dispatchEvent(event);
+  }
+}
+
+export default AmountWidget;
