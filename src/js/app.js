efd3b0934736d64fb5f1fceac9dd709e24b780ac
Dominik Krawczyk <dominix.kr@gmail.com>
2022-04-25 19:20:47 +0200

    add&handle subpage booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,68 @@
-import {settings, select } from './settings.js';
+import {settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/booking.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+    
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navlinks = document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/', '');
+  
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navlinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute*/
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        
+        /* run thisApp.activatePage with that id */ 
+
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+
+        window.location.hash = '#/' + id;
+      });
+    }
+
+
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove fron non-matching */
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /* add class "active" to matching links, remove fron non-matching */
+
+    for(let link of thisApp.navlinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+
+  },
+
   initMenu: function(){
     const thisApp = this;
 
@@ -61,10 +121,13 @@ const app = {
     // console.log('settings:', settings);
     // console.log('templates:', templates);
 
+    thisApp.initPages();
     thisApp.initData();
     // thisApp.initMenu();
     thisApp.initCart();
   },
+
+  
 };
 
 app.init();
