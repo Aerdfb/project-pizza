e73461605c681cc639abe850767007905ff05206
Dominik Krawczyk <dominix.kr@gmail.com>
2022-05-12 19:46:43 +0200

    fix to path in server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* global require, process */
 
 const jsonServer = require('json-server');
@@ -14,20 +13,3 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port);
-=======
-/* global require, process */
-
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('dist/db/app.json');
-const middlewares = jsonServer.defaults({
-  static: 'dist',
-  noCors: true
-});
-const port = process.env.PORT || 3131;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
->>>>>>> 88201754df4a87262e83479af0e49b5455236752
